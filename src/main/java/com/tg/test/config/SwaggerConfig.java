4924dd9262a9b6fb525428f577ee34c05ae40158
sclin0323 <sclin0323@gmail.com>
2026-06-04 15:08:45 +0800

    update

--- src/main/java/com/tg/test/config/SwaggerConfig.java
+++ src/main/java/com/tg/test/config/SwaggerConfig.java
@@ -1,10 +1,8 @@
 package com.tg.test.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.tags.Tag;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -25,9 +23,6 @@ public class SwaggerConfig {
                         new Tag().name("申請單作業").description("申請單的建立、提交、審核流程"),
                         new Tag().name("傳票管理作業").description("將多張申請單打包成傳票"),
                         new Tag().name("付款作業").description("對傳票執行付款流程")
-                ))
-                .servers(List.of(
-                        new Server().url("http://localhost:8080").description("本地開發環境")
                 ));
     }
 }
